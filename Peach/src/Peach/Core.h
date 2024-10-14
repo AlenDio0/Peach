282c025d5008d04a70f9744dc448c86847a46fc7
AlenDio0 <alendio.0000@gmail.com>
2024-10-14 07:51:50 +0000

    Added Ref

--- Peach/src/Peach/Core.h
+++ Peach/src/Peach/Core.h
@@ -12,3 +12,11 @@
 
 #define PEACH_ASSERT(x, ...) { if (!(x)) { PEACH_CORE_ERROR("Asserzione fallita: {}", __VA_ARGS__); } }
 #define PEACH_RETURN_ASSERT(x, ...) { if (!(x)) { PEACH_CORE_ERROR("Asserzione fallita: {}", __VA_ARGS__); return false; } return true; }
+
+template<typename T>
+using Ref<T> = std::shared_ptr<T>;
+
+template<typename T>
+Ref<T> MakeRef()
+{
+    return std::make_shared<T>();}
