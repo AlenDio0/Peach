affc43e572dc1641ebda4333e00e1408976aa296
AlenDio0 <alendio.0000@gmail.com>
2024-10-14 08:02:16 +0000

    Fixed Ref & Added Scope

--- Peach/src/Peach/Core.h
+++ Peach/src/Peach/Core.h
@@ -13,10 +13,23 @@
 #define PEACH_ASSERT(x, ...) { if (!(x)) { PEACH_CORE_ERROR("Asserzione fallita: {}", __VA_ARGS__); } }
 #define PEACH_RETURN_ASSERT(x, ...) { if (!(x)) { PEACH_CORE_ERROR("Asserzione fallita: {}", __VA_ARGS__); return false; } return true; }
 
-template<typename T>
-using Ref<T> = std::shared_ptr<T>;
-
-template<typename T>
-Ref<T> MakeRef()
+namespace Peach
 {
-    return std::make_shared<T>();}
+    template<typename T>
+    using Ref = std::shared_ptr<T>;
+
+    template<typename T, typename... Args>
+    Ref<T> MakeRef(Args&& ... args)
+    {
+        return std::make_shared<T>(std::forward<Args>(args)...);
+    }
+
+    template<typename T>
+    using Scope = std::unique_ptr<T>;
+
+    template<typename T, typename... Args>
+    Scope<T> MakeScope(Args&& ... args)
+    {
+        return std::make_unique<T>(std::forward<Args>(args)...);
+    }
+}
