321e01efd63287ed08474d05b9736daa30c6fe82
AlenDio0 <alendio.0000@gmail.com>
2024-10-14 10:54:17 +0000

    Update IState Ref

--- Peach/src/Peach/StateMachine.h
+++ Peach/src/Peach/StateMachine.h
@@ -9,24 +9,21 @@
 
 namespace Peach
 {
-	using IStateRef = std::shared_ptr<IState>;
-	using IStateStack = std::stack<IStateRef>;
-
 	class PEACH_API StateMachine
 	{
 	public:
 		StateMachine();
 		~StateMachine();
 
-		IStateRef getCurrentState() const;
+		Ref<IState> getCurrentState() const;
 
 		void update();
 
-		void addState(IStateRef newState, bool isReplacing);
+		void addState(Ref<IState> newState, bool isReplacing);
 		void removeState();
 	private:
-		IStateStack m_States;
-		IStateRef m_NewState;
+		std:stack<Ref<IState>> m_States;
+		Ref<IState> m_NewState;
 
 		bool m_IsAdding;
 		bool m_IsReplacing;
