76bd4698376050f0c2a45908940f31d101636cf2
AlenDio0 <alendio.0000@gmail.com>
2024-10-19 10:57:35 +0000

    Added Data functions into Application

--- Peach/src/Peach/Application.h
+++ Peach/src/Peach/Application.h
@@ -13,12 +13,18 @@ namespace Peach
 		Application();
 		virtual ~Application();
 
-		void addState(Ref<IState> newState, bool isReplacing);
-		void removeState();
-
 		void run();
 	protected:
 		Ref<Data> m_Data;
+	protected:
+		void addState(Ref<IState> newstate, bool replacing);
+		void removeState();
+
+		template<typename T>
+		void loadAsset(const AssetKey& key, const std::string& path, bool force = true)
+		{
+			m_Data->assets.loadAsset(key, path, force);
+		}
 	};
 
 	//To be defined in CLIENT
