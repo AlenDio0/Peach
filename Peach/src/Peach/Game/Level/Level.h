10edb2ba2f4bd5356d9b2d1e9752e05426f59991
AlenDio0 <alendio.0000@gmail.com>
2024-11-04 17:43:15 +0100

    Splitted convertFile() into various functions

--- Peach/src/Peach/Game/Level/Level.h
+++ Peach/src/Peach/Game/Level/Level.h
@@ -20,5 +20,17 @@ namespace Peach
 		void render(sf::RenderTarget* target) const;
 	private:
 		TileMap m_TileMap;
+	private:
+		void fileToTileMap(std::fstream& file);
+		void fileToMap(std::fstream& file);
+
+		bool isEnd(const std::string& buff) const;
+
+		bool find(const std::string& buff, const std::string& find) const;
+
+		uint32_t nextUInt(const std::string& buff) const;
+
+		Vec2u stringToVec2u(const std::string& str) const;
+		Vec2f stringToVec2f(const std::string& str) const;
 	};
 }
