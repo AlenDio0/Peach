240a06234929c2db140f01bdc4adb0d9fd4837e5
AlenDio0 <alendio.0000@gmail.com>
2024-11-30 08:23:23 +0000

    Updated Level

---
+++ Peach/src/Peach/Game/Level/Level.cpp
@@ -0,0 +1,124 @@
+#include "peachpch.h"
+#include "Level.h"
+
+namespace Peach
+{
+    Level::Level()
+        : Level(std::move(TileMap()))
+    {
+    }
+
+    Level::Level(TileMap&& tilemap)
+        : m_TileMap(tilemap)
+    {
+    }
+
+    Level::~Level()
+    {
+		PEACH_CORE_TRACE("Level distrutto");
+
+		PEACH_CORE_TRACE("[Level] {} Entity distrutti", m_Entities.size());
+		m_Objects.clear();
+    }
+
+    UUID Level::addEntity(Ref<Entity> entity)
+    {
+        UUID uuid;
+        
+		PEACH_CORE_TRACE("Level::addEntity(entity: {}), [uuid: {}]", entity, uuid);
+		if (!entity)
+		{
+			PEACH_CORE_ERROR("Level::addEntity(...), Impossibile aggiungere un Entity nullo");
+			return 0;
+		}
+
+        m_Entities[uuid] = entity;
+
+        return uuid;
+    }
+
+    UUID Level::addEntity(Entity* entity)
+    {
+        return addEntity(Ref<Entity>(entity));
+    }
+
+    void Level::removeEntity(UUID uuid)
+    {
+		PEACH_CORE_TRACE("Level::removeEntity(uuid: {})", uuid);
+		m_Entities.erase(uuid);
+    }
+
+    void Level::removeEntity(Ref<Entity> entity)
+    {
+		PEACH_CORE_TRACE("Level::removeEntity(entity: {})", entity);
+		for (auto& [uuid, ent] : m_Objects)
+		{
+			if (ent == entity)
+			{
+				removeEntity(uuid);
+				break;
+			}
+		}
+    }
+
+    void Level::removeEntity(Entity* entity)
+    {
+        removeEntity(Ref<Entity>(entity));
+    }
+
+    const TileMap& Level::getTileMap() const
+    {
+        return m_TileMap;
+    }
+
+    TileMap& Level::getTileMap()
+    {
+        return m_TileMap;
+    }
+
+    const EntityMap& Level::getEntities() const
+    {
+        return m_Entities;
+    }
+
+    EntityMap& Level::getEntities()
+    {
+        return m_Entities;
+    }
+
+    std::weak_ptr<Entity> Level::getEntity(UUID uuid) const
+    {
+		try
+		{
+			return m_Entities.at(uuid);
+		}
+		catch (const std::exception& e)
+		{
+			PEACH_CORE_ERROR("Level::getEntity(uuid: {}), Catturata eccezione: {}", uuid, e.what());
+			return Ref<Entity>(nullptr);
+		}
+    }
+
+    void Level::update()
+    {
+        m_TileMap.update();
+
+        for (auto& [uuid, entity] : m_Entities)
+        {
+            entity->update();
+        }
+    }
+
+    void Level::render(sf::RenderTarget* target) const
+    {
+        m_TileMap.render(target);
+
+        for (auto& [uuid, entity] : m_Entities)
+        {
+            if (auto gameobj = std::dynamic_pointer_cast<GameObject>(entity))
+            {
+                target->draw(gameobj);
+            }
+        }
+    }
+}
--- Peach/src/Peach/Game/Level/Level.h
+++ Peach/src/Peach/Game/Level/Level.h
@@ -9,24 +9,31 @@
 
 namespace Peach
 {
+    using EntityMap = std::unordered_map<UUID, Ref<Entity>>;
     class PEACH_API Level
     {
     public:
         Level();
         Level(TileMap&& tilemap);
-        
+        virtual ~Level();
+
         UUID addEntity(Ref<Entity> entity);
-        void removeEntity(Ref<Entity> entity);
+        UUID addEntity(Entity* entity);
+
         void removeEntity(UUID uuid);
+        void removeEntity(Ref<Entity> entity);
+        void removeEntity(Entity* entity);
 
         const TileMap& getTileMap() const;
         TileMap& getTileMap();
+        const EntityMap& getEntities() const;
+        EntityMap& getEntities();
+        std::weak_ptr<Entity> getEntity(UUID uuid) const;
 
-
-        void update();
-        void render(sf::RenderTarget* target) const;
+        virtual void update();
+        virtual void render(sf::RenderTarget* target) const;
     private:
         TileMap m_TileMap;
-        std::unordered_map<UUID, Ref<Entity>> m_Entities;
+        EntityMap m_Entities;
     }
 }
