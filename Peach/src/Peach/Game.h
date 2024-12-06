07c9f43707029c3e26fecef45db510c1a46c9658
AlenDio0 <alendio.0000@gmail.com>
2024-12-06 20:05:58 +0100

    Added Entity in Game.h

--- Peach/src/Peach/Game.h
+++ Peach/src/Peach/Game.h
@@ -1,8 +1,11 @@
 #pragma once
 
+#include "Game/Entity/Entity.h"
 #include "Game/GameObject.h"
 
 #include "Game/Level/Tile/TileMap.h"
 #include "Game/Level/Tile/TileMapParser.h"
+
+#include "Game/Entity/EntityManager.h"
 
 #include "Game/Level/Level.h"
