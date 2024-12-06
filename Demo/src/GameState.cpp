1ced3266d49844792a7db5e612311bb96f5728d0
AlenDio0 <alendio.0000@gmail.com>
2024-12-06 15:56:09 +0100

    Added Memory leak tests

--- Demo/src/GameState.cpp
+++ Demo/src/GameState.cpp
@@ -39,70 +39,22 @@ void GameState::onRender()
 
 void GameState::initBinds()
 {
-	/*
-	const auto& tilesize = m_Map.getTileSize();
-	const auto& mapsize = m_Map.getSize();
-	*/
 	m_Controller.bind(sf::Keyboard::Escape,
 		[&]() {
 			removeState();
 		}, "Rimuove lo Stato attuale", true);
-	/*
-	m_Controller.bind(sf::Keyboard::Num1,
+	m_Controller.bind(sf::Keyboard::A,
 		[&]() {
-			m_Map.setTileSize({ tilesize.x + 1.f, tilesize.y + 1.f });
-		}, "Aumenta la grandezza dei tile", true);
-	m_Controller.bind(sf::Keyboard::Num2,
-		[&]() {
-			m_Map.setTileSize({ tilesize.x - 1.f, tilesize.y - 1.f });
-		}, "Diminuisce la grandezza dei tile", true);
-	m_Controller.bind(sf::Keyboard::Num3,
-		[&]() {
-			m_Map.setSize({ mapsize.x + 1, mapsize.y });
-		}, "Aumenta la mappa verso destra", true);
-	m_Controller.bind(sf::Keyboard::Num4,
-		[&]() {
-			m_Map.setSize({ mapsize.x - 1, mapsize.y });
-		}, "Diminuisce la mappa verso sinistra", true);
-	m_Controller.bind(sf::Keyboard::Num5,
-		[&]() {
-			m_Map.setSize({ mapsize.x, mapsize.y + 1 });
-		}, "Aumenta la mappa verso il basso", true);
-	m_Controller.bind(sf::Keyboard::Num6,
-		[&]() {
-			m_Map.setSize({ mapsize.x, mapsize.y - 1 });
-		}, "Diminuisce la mappa verso l'alto", true);
-	m_Controller.bind(sf::Keyboard::Up,
-		[&]() {
-			m_View.width += 1;
-			m_View.height += 1;
-		}, "Aumenta la grandezza della visuale", true);
-	m_Controller.bind(sf::Keyboard::Down,
-		[&]() {
-			if (m_View.width == 0)
+			for (size_t i = 0; i < 1000; i++)
 			{
-				return;
+				auto tile = new Peach::Tile(getAsset<Peach::Texture>("tiles"), nullptr);
+				tile->setID(10);
+				m_Level.getEntityManager().addEntity(tile);
 			}
-			m_View.width -= 1;
-			m_View.height -= 1;
-		}, "Diminuisce la grandezza della visuale", true);
-	m_Controller.bind(sf::Keyboard::Right,
+		}, "Test Memory leak - EntityManager", true);
+	m_Controller.bind(sf::Keyboard::B,
 		[&]() {
-			m_View.x += 1;
-			m_View.y += 1;
-		}, "Sposta la visuale verso in basso a sinistra", true);
-	m_Controller.bind(sf::Keyboard::Left,
-		[&]() {
-			m_View.x -= 1;
-			m_View.y -= 1;
-		}, "Sposta la visuale verso in alto a destro", true);
-
-	m_Controller.bind(sf::Mouse::Left,
-		[&]() {
-			m_Map.setTexture(sf::Texture());
-		}, "Rimuove la texture dei tile", true);
-	m_Controller.bind(sf::Mouse::Right,
-		[&]() {
-			m_Map.setTexture(m_Data->assets.getAsset<Peach::Texture>("TEXTURE_TILES"));
-		}, "Imposta la texture dei tile", true);*/
+			auto& tilemap = m_Level.getTileMap();
+			tilemap.setSize(tilemap.getSize() * 10);
+		}, "Test Memory leak - TileMap", true);
 }
