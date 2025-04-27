11dfba8471ff094e31d58b2fbef633daa56d1a7a
AlenDio0 <alendio.0000@gmail.com>
2025-04-27 21:00:14 +0200

    Added Timer

---
+++ Peach/src/Peach/System/Timer.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "Peach/Core/Core.h"
+
+#include <chrono>
+
+namespace Peach
+{
+	class PEACH_API Timer
+	{
+	public:
+		Timer();
+
+		void reset();
+		float elapsedTime() const;
+		float elapsedTimeMs() const;
+	private:
+		std::chrono::time_point<std::chrono::high_resolution_clock> m_Start;
+	};
+
+	class PEACH_API ScopeTimer
+	{
+	public:
+		ScopeTimer(const std::string_view name);
+		~ScopeTimer();
+
+	private:
+		std::string m_Name;
+		Timer m_Timer;
+	};
+}
